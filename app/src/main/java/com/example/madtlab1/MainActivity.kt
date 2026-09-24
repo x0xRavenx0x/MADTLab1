8bf65e75f52e681dd2e7e21f7eeea99c9f089f4c
Yassine Driouich <yassinedrch@gmail.com>
2026-09-24 17:46:58 +0300

    Add button and text change functionality

--- app/src/main/java/com/example/madtlab1/MainActivity.kt
+++ app/src/main/java/com/example/madtlab1/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.madtlab1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
             insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+            textView.text = "Hello from MADT Lab 1!"
         }
     }
 }
